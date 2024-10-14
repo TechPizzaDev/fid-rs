d0f5f73061d411ea21b510e624e1d7db26277f83
TechPizzaDev <23627133+TechPizzaDev@users.noreply.github.com>
2024-10-14 21:11:55 +0200

    Use Criterion for bench

--- benches/lib.rs
+++ benches/lib.rs
@@ -1,102 +1,57 @@
-#![feature(test)]
-#![allow(clippy::unreadable_literal)]
+#[macro_use]
+extern crate criterion;
 
 extern crate fid;
 extern crate rand;
-extern crate test;
 
+use criterion::{BenchmarkId, Criterion};
 use fid::{BitVector, FID};
 use rand::{Rng, SeedableRng, StdRng};
-use test::Bencher;
 
-const TRIALS: u64 = 10000;
+const SIZES: [u64; 2] = [1_000_000, 100_000_000];
+const PERC: [f64; 3] = [0.01, 0.5, 0.99];
 
-#[bench]
-fn rank_1000000_dense(b: &mut Bencher) {
-    bench_rank1(1000000, 0.99, b);
-}
-
-#[bench]
-fn rank_1000000_normal(b: &mut Bencher) {
-    bench_rank1(1000000, 0.5, b);
-}
-
-#[bench]
-fn rank_1000000_sparse(b: &mut Bencher) {
-    bench_rank1(1000000, 0.01, b);
-}
-
-#[bench]
-fn rank_100000000_dense(b: &mut Bencher) {
-    bench_rank1(100000000, 0.99, b);
-}
-
-#[bench]
-fn rank_100000000_normal(b: &mut Bencher) {
-    bench_rank1(100000000, 0.5, b);
-}
-
-#[bench]
-fn rank_100000000_sparse(b: &mut Bencher) {
-    bench_rank1(100000000, 0.01, b);
-}
-
-#[bench]
-fn select_1000000_dense(b: &mut Bencher) {
-    bench_select1(1000000, 0.99, b);
-}
-
-#[bench]
-fn select_1000000_normal(b: &mut Bencher) {
-    bench_select1(1000000, 0.5, b);
-}
-
-#[bench]
-fn select_1000000_sparse(b: &mut Bencher) {
-    bench_select1(1000000, 0.01, b);
-}
-
-#[bench]
-fn select_100000000_dense(b: &mut Bencher) {
-    bench_select1(100000000, 0.99, b);
-}
-
-#[bench]
-fn select_100000000_normal(b: &mut Bencher) {
-    bench_select1(100000000, 0.5, b);
-}
-
-#[bench]
-fn select_100000000_sparse(b: &mut Bencher) {
-    bench_select1(100000000, 0.01, b);
-}
-
-fn bench_rank1(n: u64, p: f64, b: &mut Bencher) {
-    let mut rng: StdRng = SeedableRng::from_seed([0; 32]);
-    let mut bv = BitVector::new();
-    for _ in 0..n {
-        let b = rng.gen_bool(p);
-        bv.push(b);
-    }
-    b.iter(|| {
-        for _ in 0..TRIALS {
-            bv.rank1(rng.gen_range(0, n));
+pub fn bench_rank1(c: &mut Criterion) {
+    for (n, p) in SIZES.iter().copied().zip(PERC) {
+        let mut rng: StdRng = SeedableRng::from_seed([0; 32]);
+        let mut bv = BitVector::new();
+        for _ in 0..n {
+            let b = rng.gen_bool(p);
+            bv.push(b);
         }
-    })
+        c.bench_with_input(
+            BenchmarkId::new("rank1", format!("{:?}", (n, p))),
+            &bv,
+            move |b, bv| {
+                b.iter(|| {
+                    bv.rank1(rng.gen_range(0, n));
+                })
+            },
+        );
+    }
 }
 
-fn bench_select1(n: u64, p: f64, b: &mut Bencher) {
-    let mut rng: StdRng = SeedableRng::from_seed([0; 32]);
-    let mut bv = BitVector::new();
-    let mut rank = 0;
-    for _ in 0..n {
-        let b = rng.gen_bool(p);
-        bv.push(b);
-        rank += b as u64;
-    }
-    b.iter(|| {
-        for _ in 0..TRIALS {
-            bv.select1(rng.gen_range(0, rank));
+pub fn bench_select1(c: &mut Criterion) {
+    for (n, p) in SIZES.iter().copied().zip(PERC) {
+        let mut rng: StdRng = SeedableRng::from_seed([0; 32]);
+        let mut bv = BitVector::new();
+        let mut rank = 0;
+        for _ in 0..n {
+            let b = rng.gen_bool(p);
+            bv.push(b);
+            rank += b as u64;
         }
-    })
+        c.bench_with_input(
+            BenchmarkId::new("rank1", format!("{:?}", (n, p))),
+            &bv,
+            |b, bv| {
+                b.iter(|| {
+                    bv.select1(rng.gen_range(0, rank));
+                })
+            },
+        );
+    }
 }
+
+criterion_group!(benches, bench_rank1, bench_select1);
+criterion_main!(benches);
